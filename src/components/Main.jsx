6a2f1076588a267538d9fbcf342fe59648489f2b
yatanbase <yatankulshrestha@gmail.com>
2024-05-03 13:53:01 +0530

    news panel dropshadow

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -287,70 +287,70 @@ const MainContent = () => {
 
             </div>
         </div>
-        <div className='Box-About-2' style={{ border: "1px solid black",marginTop:"10px", padding: "10px", height: "1350px" }}>
-            <div style={{ width: '100%', height: '550px',marginTop:"10px", border: '2px solid black',padding:"10px 10px" , backgroundColor:"#F0F0F0",marginBottom:"10px" ,textAlign:'ce'}}>
+        <div className='Box-About-2' style={{ marginTop:"10px", padding: "10px", height: "1333px" }}>
+            <div style={{ width: '100%', height: '550px',marginTop:"10px",  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.5)",padding:"10px 10px" , backgroundColor:"#F0F0F0",marginBottom:"10px" ,textAlign:'ce'}}>
 <h4 className='heading-news'>News and Announcements</h4>
 <br></br>
 
 <div className="news-announcement-box" style={{ display: 'flex', alignItems: 'center' }}>
-<a href="/News1" className='text-decoration-none' style={{ fontWeight: 'bold', marginRight: '10px', marginLeft: '8px',marginTop:'-20px' }}>Street Play Script Writing Contest on the Theme Disaster Management </a>
+<a href="/News1" className='text-decoration-none' style={{  marginRight: '10px', marginLeft: '8px',marginTop:'-20px' }}>Street Play Script Writing Contest on the Theme Disaster Management </a>
 
 </div>
 <br></br>
 
 <div className="news-announcement-box" style={{ display: 'flex', alignItems: 'center' }}>
-<a href="/News2" className=' text-decoration-none' style={{ fontWeight: 'bold', marginRight: '10px', marginLeft: '8px' }}>Human Rights Awareness Programme organized at SJMC</a>
+<a href="/News2" className=' text-decoration-none' style={{  marginRight: '10px', marginLeft: '8px' }}>Human Rights Awareness Programme organized at SJMC</a>
 </div>
 <br></br>
 
 <div className="news-announcement-box" style={{ display: 'flex', alignItems: 'center' }}>
-<a href="/News6" className=' text-decoration-none' style={{ fontWeight: 'bold', marginRight: '10px', marginLeft: '8px' }}>Student film recognised in renowned film festivals.</a>
+<a href="/News6" className=' text-decoration-none' style={{  marginRight: '10px', marginLeft: '8px' }}>Student film recognised in renowned film festivals.</a>
 </div>
 <br></br>
 
 <div className="news-announcement-box" style={{ display: 'flex', alignItems: 'center' }}>
-<a href="/News3" className=' text-decoration-none' style={{ fontWeight: 'bold', marginRight: '10px', marginLeft: '8px' }}>Training workshop for media professionals to create awareness about disaster mitigation.</a>
+<a href="/News3" className=' text-decoration-none' style={{  marginRight: '10px', marginLeft: '8px' }}>Training workshop for media professionals to create awareness about disaster mitigation.</a>
 </div>
 <br></br>
 
 <div className="news-announcement-box" style={{ display: 'flex', alignItems: 'center' }}>
-<a href="/News4" className='text-decoration-none' style={{ fontWeight: 'bold', marginRight: '10px', marginLeft: '8px' }}>Open Mic on "Youth Icon of Bihar" </a>
+<a href="/News4" className='text-decoration-none' style={{  marginRight: '10px', marginLeft: '8px' }}>Open Mic on "Youth Icon of Bihar" </a>
 </div>
 <br></br>
 
 <div className="news-announcement-box" style={{ display: 'flex', alignItems: 'center' }}>
 <br></br>
-<a href="/News5" className=' text-decoration-none' style={{ fontWeight: 'bold', marginRight: '10px', marginLeft: '8px' }}>Screening of seven films of SJMC students  </a>
+<a href="/News5" className=' text-decoration-none' style={{  marginRight: '10px', marginLeft: '8px' }}>Screening of seven films of SJMC students  </a>
 </div>
 
 <br></br>
 
 <div className="news-announcement-box" style={{ display: 'flex', alignItems: 'center' }}>
 <br></br>
-<a href="/News7" className=' text-decoration-none' style={{ fontWeight: 'bold', marginRight: '10px', marginLeft: '8px' }}>Patna Book Fair  </a>
+<a href="/News7" className=' text-decoration-none' style={{  marginRight: '10px', marginLeft: '8px' }}>Patna Book Fair  </a>
 </div>
 
 
 
 </div>
 
-            <div style={{ width: '100%', height: '750px', border: '2px solid black', padding:"10px 10px"  , backgroundColor:"#F0F0F0" }}>
+            <div style={{ width: '100%', height: '750px',boxShadow:'0 4px 8px rgb(0,0,0,0.5)', padding:"10px 10px"  , backgroundColor:"#F0F0F0" }}>
                 <h4 className='heading-news'>Important Links</h4>
 
                 <div className="news-announcement-box" style={{ display: 'flex', alignItems: 'center',margin:'20px 0px 20px 0px' }}><br></br>
-<a href={Announcement1} target='_blank' className=' text-decoration-none' style={{ fontWeight: 'bold', marginRight: '10px', marginLeft: '8px' }}>Notice About Academic Advisory Committee </a>
+<a href={Announcement1} target='_blank' className=' text-decoration-none' style={{  marginRight: '10px', marginLeft: '8px' }}>Notice About Academic Advisory Committee </a>
 </div>
 
 <div className="news-announcement-box" style={{ display: 'flex', alignItems: 'center' ,margin:'20px 0px 20px 0px'}}><br></br>
-<a href={Announcement2} target='_blank' className=' text-decoration-none' style={{ fontWeight: 'bold', marginRight: '10px', marginLeft: '8px' }}>Board Of Studies  </a>
+<a href={Announcement2} target='_blank' className=' text-decoration-none' style={{  marginRight: '10px', marginLeft: '8px' }}>Board Of Studies  </a>
 </div>
 
 <div className="news-announcement-box" style={{ display: 'flex', alignItems: 'center' ,margin:'20px 0px 20px 0px'}}><br></br>
-<a href={Announcement3} target='_blank' className=' text-decoration-none' style={{ fontWeight: 'bold', marginRight: '10px', marginLeft: '8px' }}>Approved Syllabus Of Courses Offered By SJMC  </a>
+<a href={Announcement3} target='_blank' className=' text-decoration-none' style={{  marginRight: '10px', marginLeft: '8px' }}>Approved Syllabus Of Courses Offered By SJMC  </a>
 </div>
 
 <div className="news-announcement-box" style={{ display: 'flex', alignItems: 'center' ,margin:'20px 0px 20px 0px'}}><br></br>
-<a href={Announcement4} target='_blank' className=' text-decoration-none' style={{ fontWeight: 'bold', marginRight: '10px', marginLeft: '8px' }}>MoU between SJMC & BIPARD  </a>
+<a href={Announcement4} target='_blank' className=' text-decoration-none' style={{  marginRight: '10px', marginLeft: '8px' }}>MoU between SJMC & BIPARD  </a>
 </div>
 
 
