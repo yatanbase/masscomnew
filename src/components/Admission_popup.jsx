9b64532c23cf25a096a8d9bcde36c64845d2cf22
yatanbase <yatankulshrestha@gmail.com>
2025-05-27 21:41:44 +0530

    minor suggestive comment

--- src/components/Admission_popup.jsx
+++ src/components/Admission_popup.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import './ad_popup.css'; // Import the CSS for styling
 import Image1 from '../asset/popup (1).jpg'
 // import Image2 from '../asset/popup (2).jpg'
-import advideo from '../asset/skill courses.mp4'
+import advideo from '../asset/skill courses.mp4' //videos should be uploaded on CDN and then used here to reduce build size
 
 const Admission_popup = ({ onClose }) => {
   const adRef = useRef(null);
