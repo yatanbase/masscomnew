9a0376df4c22aa3b7ca1d382455c59d38d6a18f5
Binit Kumar <binit491@gmail.com>
2025-07-03 11:00:45 +0530

    Update Admission_popup.jsx

--- src/components/Admission_popup.jsx
+++ src/components/Admission_popup.jsx
@@ -40,7 +40,7 @@ const Admission_popup = ({ onClose }) => {
             <source src = {advideo} type="video/mp4" />
             </video>
           <div className="button_container">
-            <a href="https://adms.akubihar.ac.in/MMSY.aspx" target='_blank' className="primary-button">Click for Details</a>
+            <a href="https://adms.akubihar.ac.in/" target='_blank' className="primary-button">Click for Details</a>
           </div>
           <img src={Image1} alt="Advertisement 1" className="ad-image" />
           <div className="button_container">
