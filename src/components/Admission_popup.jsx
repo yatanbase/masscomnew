ea946d59fe1484813415513ec5a0edd67cdf6203
Binit Kumar <binit491@gmail.com>
2025-08-08 18:17:36 +0530

    upcomming event

--- src/components/Admission_popup.jsx
+++ src/components/Admission_popup.jsx
@@ -7,7 +7,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation'; // ✅ Navigation CSS
 
 import advideo from '../asset/skill course.mp4';
-import Image5 from '../asset/Adms_banner5.jpg';
+import Image5 from '../asset/Adms_banner5.jpeg';
 import Image1 from '../asset/Adms_banner4.jpeg';
 import Image2 from '../asset/Adms_banner1.jpg';
 import Image3 from '../asset/Adms_banner2.jpg';
@@ -70,7 +70,7 @@ const Admission_popup = ({ onClose }) => {
               <img src={Image4} alt="Ad 4" className="ad-image" />
             </SwiperSlide>
             <SwiperSlide>
-              <img src={Image5} alt="Ad 4" className="ad-image" />
+              <img src={Image5} alt="Ad 5" className="ad-image" />
             </SwiperSlide>
           </Swiper>
 
