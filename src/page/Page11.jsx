aba810b6127c1ef6e041299371083bcb3074a24e
Binit Kumar <binit491@gmail.com>
2025-10-29 12:14:15 +0530

    Update Page11.jsx
    pointing wise

--- src/page/Page11.jsx
+++ src/page/Page11.jsx
@@ -19,19 +19,19 @@ function Page11() {
       <h2 style={{textAlign: 'left', borderBottom: '1px solid black'}}><a href="#" style={{color: 'black' , textDecoration: 'none'}}>Mision</a></h2>
       <br />
       <div>
-  <p style={{ fontSize: '20px' }}>To establish a state-of-the-art facility for teaching and research on mass communication.</p>
-  <p style={{ fontSize: '20px' }}>To conduct academic courses at Postgraduate and Research levels.</p>
-  <p style={{ fontSize: '20px' }}>To provide excellent supervision to scholars to undertake research in journalism & mass communication and allied topics.</p>
-  <p style={{ fontSize: '20px' }}>To conduct researches on various aspects of journalism & mass communication.</p>
-  <p style={{ fontSize: '20px' }}>To contribute and enrich knowledge on journalism & mass communication.</p>
-  <p style={{ fontSize: '20px' }}>To anticipate the technological and scientific needs for the state and to plan and prepare to cater them.</p>
-  <p style={{ fontSize: '20px' }}>To develop programs for faculty development for its own staff and for teachers and researchers of other institutions with similar objectives.</p>
-  <p style={{ fontSize: '20px' }}>To collaborate with various faculties / schools of AKU to meet its objectives.</p>
-  <p style={{ fontSize: '20px' }}>To collaborate with regional, national and international institutions to fulfill its visions, accomplish its missions and meet its objectives.</p>
-  <p style={{ fontSize: '20px' }}>To provide consultancy services in the field of its expertise to government and other agencies.</p>
-  <p style={{ fontSize: '20px' }}>To organize training programmes for professionals, bureaucrats and civil society members on media management and associated aspects.</p>
-  <p style={{ fontSize: '20px' }}>To cater to the development of a culture for preservation and conservation of media ethics.</p>
-  <p style={{ fontSize: '20px' }}>To leverage its key resources.</p>
+  <p style={{ fontSize: '20px' }}>* To establish a state-of-the-art facility for teaching and research on mass communication.</p>
+  <p style={{ fontSize: '20px' }}>* To conduct academic courses at Postgraduate and Research levels.</p>
+  <p style={{ fontSize: '20px' }}>* To provide excellent supervision to scholars to undertake research in journalism & mass communication and allied topics.</p>
+  <p style={{ fontSize: '20px' }}>* To conduct researches on various aspects of journalism & mass communication.</p>
+  <p style={{ fontSize: '20px' }}>* To contribute and enrich knowledge on journalism & mass communication.</p>
+  <p style={{ fontSize: '20px' }}>* To anticipate the technological and scientific needs for the state and to plan and prepare to cater them.</p>
+  <p style={{ fontSize: '20px' }}>* To develop programs for faculty development for its own staff and for teachers and researchers of other institutions with similar objectives.</p>
+  <p style={{ fontSize: '20px' }}>* To collaborate with various faculties / schools of AKU to meet its objectives.</p>
+  <p style={{ fontSize: '20px' }}>* To collaborate with regional, national and international institutions to fulfill its visions, accomplish its missions and meet its objectives.</p>
+  <p style={{ fontSize: '20px' }}>* To provide consultancy services in the field of its expertise to government and other agencies.</p>
+  <p style={{ fontSize: '20px' }}>* To organize training programmes for professionals, bureaucrats and civil society members on media management and associated aspects.</p>
+  <p style={{ fontSize: '20px' }}>* To cater to the development of a culture for preservation and conservation of media ethics.</p>
+  <p style={{ fontSize: '20px' }}>* To leverage its key resources.</p>
 </div>
 
     </div>
